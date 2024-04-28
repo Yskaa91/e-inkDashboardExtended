f2b1f98256bcb7194c6506709ac1cd1da5a5895f
Jasper <jasperkouwenberg@gmail.com>
2024-04-28 15:09:43 +0200

    minor changes

--- public/internationalisation.js
+++ public/internationalisation.js
@@ -16,10 +16,10 @@ var dashboardCollisionAlarmTXT = 'Distance to the nearest navigate danger';
 var dashboardMOBalarmTXT = 'Distance to the MOB';
 var dashboardAlarmDistanceMesTXT = 'm.'
 
-var dashboardWindSpeedTXT = 'Apparent wind speed';
+var dashboardWindSpeedTXT = 'App. wind speed';
 var dashboardWindSpeedMesTXT = 'kn';
 var dashboardTrueWindSpeedTXT = 'True wind speed';
-var dashboardWindAngleTXT = 'Apparent wind angle';
+var dashboardWindAngleTXT = 'App. wind angle';
 var dashboardTrueWindAngleTXT = 'True wind angle';
 var dashboardWindAngleMesTXT = 'deg';
 
